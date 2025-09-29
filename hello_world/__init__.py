5b2c545c98938c3c8288e0aab48a794e1c6fa05c
ChiragChrg <chiruchirag2001@gmail.com>
2025-09-29 12:32:25 +0530

    refactor: ♻️ Remove unused hello_world package and scripts

--- hello_world/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes the hello_world directory a Python package
--- hello_world/hello.py
+++
@@ -1,6 +0,0 @@
-def display_message():
-    """Display Hello Aider message"""
-    print("Hello Aider")
-
-if __name__ == "__main__":
-    display_message()
